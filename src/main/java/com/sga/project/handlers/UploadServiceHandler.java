d9494fd04fada25efc8a278a12d6f93ef79971c0
Jitendra Selvam <jitendra3195@gmail.com>
2018-09-17 18:16:06 -0400

    fixed kafka

--- src/main/java/com/sga/project/handlers/UploadServiceHandler.java
+++ src/main/java/com/sga/project/handlers/UploadServiceHandler.java
@@ -1,7 +1,11 @@
 package com.sga.project.handlers;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.RandomAccessFile;
 
 import org.apache.thrift.TException;
@@ -17,13 +21,12 @@ import com.sga.project.services.*;
 public class UploadServiceHandler implements UploadService.Iface{
 	
 	@Autowired
-    private KafkaTemplate<String, TransferInfo> senderHelper;
+    private KafkaTemplate<String, String> senderHelper;
 
 	private static final File BASE_DIRECTORY = new File("downloads");
+	private static final File SHARED_DIRECTORY = new File("D:\\sharedDirectory");
 	
 	private Context context;
-	
-	
 	
 	@Override
 	public void upload(TransferInfo info) throws TException {
@@ -67,12 +70,12 @@ public class UploadServiceHandler implements UploadService.Iface{
 	
 	private void progressUpload(TransferInfo info) throws TException {
         try {
-        	senderHelper.send("topic1",info);
             context.raf.getChannel().write(info.data, context.raf.length());
             if (context.file.length() == context.length) {
                 context.raf.close();
+                copyFileUsingStream(context);
+                senderHelper.send("topic1",copyFileUsingStream(context));
             }
-            //senderHelper.send("topic1",context.file.getName());
         } catch (IOException e) {
             try {
                 context.raf.close();
@@ -82,9 +85,30 @@ public class UploadServiceHandler implements UploadService.Iface{
             context.file.delete();
             throw new TException(e);
         }
-        
     }
 	
+	private String copyFileUsingStream(Context ctx) throws IOException {
+		File source = new File(BASE_DIRECTORY+"\\"+ctx.file.getName());
+        File dest = new File(SHARED_DIRECTORY+"\\"+ctx.file.getName());
+        
+	    InputStream is = null;
+	    OutputStream os = null;
+	    try {
+	        is = new FileInputStream(source);
+	        os = new FileOutputStream(dest);
+	        byte[] buffer = new byte[1024];
+	        int length;
+	        while ((length = is.read(buffer)) > 0) {
+	            os.write(buffer, 0, length);
+	        }
+	    } finally {
+	        is.close();
+	        os.close();
+	    }
+	    
+	    return ctx.file.getName();
+	}
+
 	private static class Context {
         private File file;
 
