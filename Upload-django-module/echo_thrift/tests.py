e90dc8aca802183ea0e8b639c55942ac6a184652
Soutri Mukherjee <soutri94@gmail.com>
2018-09-18 16:04:29 -0400

    adding requirements.txt

--- Upload-django-module/echo_thrift/tests.py
+++ Upload-django-module/echo_thrift/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+
+class MyTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up data for the whole TestCase
+        cls.foo = Foo.objects.create(bar="Test")
+        ...
+
+    def test1(self):
+        # Some test using self.foo
+        ...
+
+    def test2(self):
+        # Some other test using self.foo
+
+        ...
+
+    import unittest
+    from django.test
+
+    class SimpleTest(unittest.TestCase):
+        def test_details(self):
+            client = Client()
+            response = client.get('/customer/details/')
+            self.assertEqual(response.status_code, 200)
+
+        def test_index(self):
+            client = Client()
+            response = client.get('/customer/index/')
+            self.assertEqual(response.status_code, 200)
