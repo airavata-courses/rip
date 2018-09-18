ce52f0f90bd668956196405fe1b9d0ac5c039ea1
Soutri Mukherjee <soutri94@gmail.com>
2018-09-18 17:15:48 -0400

    Added Test Cases

--- Upload-django-module/echo_thrift/tests.py
+++ Upload-django-module/echo_thrift/tests.py
@@ -1,6 +1,6 @@
 
 # Create your tests here.
-from django.test import TestCase
+'''from django.test import TestCase
 import unittest
 
 class MyTests(TestCase):
@@ -19,7 +19,7 @@ class MyTests(TestCase):
 
 
 
-  '''  class SimpleTest(unittest.TestCase):
+    class SimpleTest(unittest.TestCase):
         def test_details(self):
             client = Client()
             response = client.get('/customer/details/')
@@ -28,5 +28,17 @@ class MyTests(TestCase):
         def test_index(self):
             client = Client()
             response = client.get('/customer/index/')
-            self.assertEqual(response.status_code, 200)
-'''
+            self.assertEqual(response.status_code, 200)'''
+
+
+from django.contrib.auth.models import AnonymousUser, User
+from django.test import TestCase
+
+class SearchFormTestCase(TestCase):
+    def test_empty_get(self):
+        response = self.client.get('/echo_thrift', HTTP_HOST='http://127.0.0.1:8000')
+        self.assertEqual(response.status_code, 400)
+
+    def test_empty_get1(self):
+            response = self.client.get('/en/dev/search/', HTTP_HOST='docs.djangoproject.dev:8000')
+            self.assertEqual(response.status_code, 400)
