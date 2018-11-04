8ebf4a836b1c01562e770c0a110331ae519606f7
Jitendra Selvam <jselvam@iu.edu>
2018-11-04 15:51:54 -0500

    Update fileCreate.sh

--- fileCreate.sh
+++ fileCreate.sh
@@ -3,11 +3,6 @@ if [ ! -e "/bin/sharedDirectory/" ]; then
     sudo mkdir "/bin/sharedDirectory/"
 fi
 
-if [ ! -e "/sga/" ]; then
-    echo "sga directory does not exist, creating a new sga directory."
-    sudo mkdir "/sga/"
-else
-    rm -rf sga
-fi
+
 
 
