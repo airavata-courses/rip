0c050dcdf565c18a39fa9092723d1c5eaf4a8e4a
Jitendra Selvam <jselvam@iu.edu>
2018-11-04 14:57:58 -0500

    Update fileCreate.sh

--- fileCreate.sh
+++ fileCreate.sh
@@ -1,5 +1,6 @@
 if [ ! -e "/bin/sharedDirectory/" ]; then
-    pwd
     echo "shared directory does not exist, creating a new shared directory."
     sudo mkdir "/bin/sharedDirectory/"
 fi
+
+mkdir sga
